71047ef25d48545720a18359c6753947ad885094
exten113 <1259592337@qq.com>
2022-04-15 23:49:54 +0800

    添加获取邮件验证码和账号找回的接口。 更改注册接口，在其中添加了验证码验证的部分

--- surongdan/views.py
+++ surongdan/views.py
@@ -1,9 +1,16 @@
-from flask import request, jsonify, session, make_response 
-
-from surongdan import app, db
-from surongdan.models import user_table 
-
+import datetime
+import random
 import re
+
+from flask import request, jsonify, session, make_response
+
+from surongdan import db
+from surongdan.mail import *
+from surongdan.models import user_table
+
+# 存放用户对应的验证码
+mails_verified_list = {}
+
 
 ############ 设置路由 ###########
 # 首页，欢迎
@@ -17,28 +24,74 @@ def index():
         response += '[Not Authenticated]'
     return response
 
+
+@app.route('/users/get_verified', methods={'POST'})
+def verified():
+    data = request.get_json()
+    code = ''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890') for _ in range(6))
+    user_email = data['user_email']
+    send_mail(user_email, '验证码', '您的验证码为' + code)
+    # 记录验证码信息，前一个为验证码，后一个为验证码发出时的时间
+    mails_verified_list[user_email] = (code, datetime.datetime.now())
+    return "", 200
+
+
 # 负责处理注册请求
 @app.route('/users/register', methods={'POST'})
 def register():
     data = request.get_json()
-    #print(data)    
-    #print(data['user_name'])
+    # print(data)
+    # print(data['user_name'])
     # 判断name是否重复
-    if(user_table.query.filter_by(user_name = data['user_name']).first()):
-        return jsonify({'fault':'user_name is invalid!'}), 202
+    if user_table.query.filter_by(user_name=data['user_name']).first():
+        return jsonify({'fault': 'user_name is invalid!'}), 202
     # 判断email是否重复
-    if(user_table.query.filter_by(user_email = data['user_email']).first()):
-        return jsonify({'fault':'user_email is invalid!'}), 202
-    u = user_table( user_name = data['user_name'],
-                    user_email = data['user_email'],
-                    user_pwd = data['user_pwd'],
-                    user_status = True)
+    if user_table.query.filter_by(user_email=data['user_email']).first():
+        return jsonify({'fault': 'user_email is invalid!'}), 202
+    # 判断验证码是否正确
+    if data['user_verify'] != mails_verified_list[data['user_email']][0]:
+        return jsonify({'fault': 'user_verify is invalid!'}), 202
+    # 判断验证码是否超时，超过十分钟则失效
+    min = (datetime.datetime.now() - mails_verified_list[data['user_email']][1]).seconds / 60
+    if min > 10:
+        return jsonify({'fault': 'user_verify is overdue!'}), 202
+
+    u = user_table(user_name=data['user_name'],
+                   user_email=data['user_email'],
+                   user_pwd=data['user_pwd'],
+                   user_status=True)
     db.session.add(u)
     db.session.commit()
-    if(u.user_id == None):
-        return jsonify({'fault':'something wrong'}), 202 
+    if (u.user_id == None):
+        return jsonify({'fault': 'something wrong'}), 202
     else:
-        return jsonify({'user_id':u.user_id, 'user_name':u.user_name}), 201
+        return jsonify({'user_id': u.user_id, 'user_name': u.user_name}), 201
+
+
+# 找回密码
+@app.route('/users/findback', methods={'POST'})
+def find_back():
+    data = request.get_json()
+    # 判断email是否存在
+    u = user_table.query.filter_by(user_email=data['user_email']).first()
+    if u is None:
+        return jsonify({'fault': 'user_email is invalid!'}), 202
+    # 判断验证码是否正确
+    if data['user_verify'] != mails_verified_list[data['user_email']][0]:
+        return jsonify({'fault': 'user_verify is invalid!'}), 202
+    # 判断验证码是否超时，超过十分钟则失效
+    min = (datetime.datetime.now() - mails_verified_list[data['user_email']][1]).seconds / 60
+    if min > 10:
+        return jsonify({'fault': 'user_verify is overdue!'}), 202
+
+    u.user_pwd = data['user_pwd']
+
+    db.session.commit()
+    if u.user_id == None:
+        return jsonify({'fault': 'something wrong'}), 202
+    else:
+        return jsonify({'user_id': u.user_id, 'user_name': u.user_name}), 201
+
 
 # 负责处理登录请求
 @app.route('/users/login', methods={'POST'})
@@ -47,26 +100,26 @@ def login():
     name = request.cookies.get('name')
     print(name)
     if name and session.get('logged_in'):
-        return jsonify({'fault':'You have already logged in'}), 202
-    
+        return jsonify({'fault': 'You have already logged in'}), 202
+
     email_str = r'^[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+){0,4}@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+){0,4}$'
     data = request.get_json()
     u = None
     if re.match(email_str, data['user_info']):
-        u = user_table.query.filter_by(user_email = data['user_info']).first()
+        u = user_table.query.filter_by(user_email=data['user_info']).first()
     else:
-        u = user_table.query.filter_by(user_name = data['user_info']).first()
+        u = user_table.query.filter_by(user_name=data['user_info']).first()
     # 用户信息检查
-    if(u == None):
-        return jsonify({'fault':'user info error!'}), 203
-    if(u.user_pwd != data['user_pwd']):
-        return jsonify({'fault':'user info error!'}), 203
-    if(u.user_status == False):
-        return jsonify({'fault':'user is invalid!'}), 204
+    if (u == None):
+        return jsonify({'fault': 'user info error!'}), 203
+    if (u.user_pwd != data['user_pwd']):
+        return jsonify({'fault': 'user info error!'}), 203
+    if (u.user_status == False):
+        return jsonify({'fault': 'user is invalid!'}), 204
 
     # 登录成功
     session['logged_in'] = True
-    response = make_response(jsonify({'user_id':u.user_id, 'user_name':u.user_name}))
+    response = make_response(jsonify({'user_id': u.user_id, 'user_name': u.user_name}))
     response.set_cookie('name', u.user_name)
     return response, 200
 
@@ -74,12 +127,11 @@ def login():
 @app.route('/users/logout', methods={'POST'})
 def logout():
     # 删除session和cookie
-    if(not session.get('logged_in')):
-        response = jsonify({'info':'invalid logout'})
+    if (not session.get('logged_in')):
+        response = jsonify({'info': 'invalid logout'})
         response.delete_cookie('name')
         return response, 202
     session.pop('logged_in')
-    response = jsonify({'info':'successful logout'})
+    response = jsonify({'info': 'successful logout'})
     response.delete_cookie('name')
     return response, 200
-
